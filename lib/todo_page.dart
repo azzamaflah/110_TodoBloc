e282a2ace81a13f6dad0eb80d68ac445b79f75fe
azzamaflah <azzamhebat46@gmail.com>
2025-05-08 23:49:33 +0700

    membuat todo page dan tulisan judul 'todo list'

---
+++ lib/todo_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TodoPage extends StatelessWidget {
+  const TodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _key = GlobalKey<FormState>();
+    final _controller = TextEditingController();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('Todo List'),
+
+              
+            ]
+          ),)),
+    );
+  }
+}

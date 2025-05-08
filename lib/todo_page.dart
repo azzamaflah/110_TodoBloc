3ff08d3de3a2a9949315b311e32efc6e2961a67d
azzamaflah <azzamhebat46@gmail.com>
2025-05-09 00:06:14 +0700

    Memperbaiki UI untuk menampilkan status loading dan menangani kondisi kosong pada daftar Todo

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -105,6 +105,18 @@ class TodoPage extends StatelessWidget {
                   ],
                 ),
               ),
+              
+            SizedBox(height: 16.0),
+              Expanded(
+                child: BlocBuilder<TodoBloc, TodoState>(
+                  builder: (context, state) {
+                    if(state is TodoLoading) {
+                      return Center(child: CircularProgressIndicator(),);
+                    } else if (state is TodoLoaded) {
+                      
+                      if(state.todos.isEmpty) {
+                        return Center(child: Text('Todo List Kosong'));
+                      }
             ],
           ),
         ),
